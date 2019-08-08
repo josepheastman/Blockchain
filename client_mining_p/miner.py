d655a3dde03cd869de278599286fc672d88eddd5
Joseph <josephryan96@gmail.com>
2019-08-07 20:37:42 -0700

    work done in client_mining_p

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -5,7 +5,31 @@ import sys
 
 
 # TODO: Implement functionality to search for a proof 
+def proof_of_work(last_proof):
+    """
+    Simple Proof of Work Algorithm
+    Find a number p such that hash(last_block_string, p) contains 6 leading
+    zeroes
+    """
+    print("Starting work on a new proof")
+    proof = 0
 
+    # for block 1, hash(1, p) = 000000x
+    while not valid_proof(last_proof, proof):
+        proof += 1
+    print("Sending request to server")
+    return proof
+
+def valid_proof(last_proof, proof):
+    guess = f'{last_proof}{proof}'.encode()
+    # ise hash function
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    # check if 6 leading 0's in hash result
+    beg = guess_hash[0:6]  # [:6]
+    if beg == "000000":
+         return True
+    else:
+        return False
 
 if __name__ == '__main__':
     # What node are we interacting with?
@@ -17,11 +41,29 @@ if __name__ == '__main__':
     coins_mined = 0
     # Run forever until interrupted
     while True:
-        # TODO: Get the last proof from the server and look for a new one
-        # TODO: When found, POST it to the server {"proof": new_proof}
-        # TODO: We're going to have to research how to do a POST in Python
+
+        # generate request with /last_proof
+        
+        # look for a new one
+        new_proof = proof_of_work(last_proof)
+        
+        #  When found, POST it to the server {"proof": new_proof}
+        # {"proof": new_proof}
+
+
+        # We're going to have to research how to do a POST in Python
         # HINT: Research `requests` and remember we're sending our data as JSON
+        data = {
+            'proof': new_proof
+        }
+
+
+        r = requests.post(url = node+'/mine', data = data)
+
         # TODO: If the server responds with 'New Block Forged'
-        # add 1 to the number of coins mined and print it.  Otherwise,
+        # add 1 to the number of coins mined (for this client) and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if r.message == "New Block Forged":
+            coins_mined +=1
+            print("You have: " + str(coins_mined) + " coins")
+        print(r.message)
